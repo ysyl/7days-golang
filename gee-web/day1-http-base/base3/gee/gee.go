897c49461c9e20f4ac03131327340ebb7ec7b733
M1 <zhoujian1237@gmail.com>
2023-03-28 11:28:43 +0800

    完成首日web框架训练

--- gee-web/day1-http-base/base3/gee/gee.go
+++ gee-web/day1-http-base/base3/gee/gee.go
@@ -1,50 +1,45 @@
 package gee
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
-// HandlerFunc defines the request handler used by gee
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
 
-// Engine implement the interface of ServeHTTP
-type Engine struct {
-	router map[string]HandlerFunc
+type SimpleEngine struct {
+    routers map[string]HandlerFunc
 }
 
-// New is the constructor of gee.Engine
-func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+func New() *SimpleEngine {
+    return &SimpleEngine{routers: map[string]HandlerFunc{}}
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	log.Printf("Route %4s - %s", method, pattern)
-	engine.router[key] = handler
+func (engine *SimpleEngine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+    method, path := request.Method, request.URL.Path
+    key := method + "-" + path
+    handlerFunc, ok := engine.routers[key]
+    if ok {
+        handlerFunc(writer, request)
+    } else {
+        fmt.Fprintf(writer, "404 not fount, "+key)
+    }
 }
 
-// GET defines the method to add GET request
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (engine SimpleEngine) addRoute(method, path string, handlerFunc HandlerFunc) {
+    key := method + "-" + path
+    engine.routers[key] = handlerFunc
 }
 
-// POST defines the method to add POST request
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (engine *SimpleEngine) GET(path string, handlerFunc HandlerFunc) {
+    engine.addRoute("GET", path, handlerFunc)
 }
 
-// Run defines the method to start a http server
-func (engine *Engine) Run(addr string) (err error) {
-	return http.ListenAndServe(addr, engine)
+func (engine *SimpleEngine) POST(path string, handlerFunc HandlerFunc) {
+    engine.addRoute("POST", path, handlerFunc)
 }
 
-func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+func (engine *SimpleEngine) Run(addr string) error {
+    err := http.ListenAndServe(addr, engine)
+    return err
 }
