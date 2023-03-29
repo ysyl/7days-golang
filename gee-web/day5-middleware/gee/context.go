0c281e6629c9947281ebdf04e21a2165e95a3840
M1 <zhoujian1237@gmail.com>
2023-03-29 23:47:24 +0800

    提交中间件逻辑

--- gee-web/day5-middleware/gee/context.go
+++ gee-web/day5-middleware/gee/context.go
@@ -4,92 +4,70 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
-type H map[string]interface{}
-
 type Context struct {
-	// origin objects
-	Writer http.ResponseWriter
-	Req    *http.Request
-	// request info
-	Path   string
-	Method string
-	Params map[string]string
-	// response info
-	StatusCode int
-	// middleware
-	handlers []HandlerFunc
-	index    int
+	Writer     http.ResponseWriter
+	Req        *http.Request
+	Path       string
+	Params     map[string]string
+	StatusCode HttpStatus
+	Before     time.Time
+	After      time.Time
 }
 
-func newContext(w http.ResponseWriter, req *http.Request) *Context {
-	return &Context{
-		Path:   req.URL.Path,
-		Method: req.Method,
-		Req:    req,
-		Writer: w,
-		index:  -1,
+type HttpStatus int
+
+func (s HttpStatus) Message() string {
+	if s == 200 {
+		return "success"
 	}
+	return ""
 }
 
-func (c *Context) Next() {
-	c.index++
-	s := len(c.handlers)
-	for ; c.index < s; c.index++ {
-		c.handlers[c.index](c)
+var (
+	SUCCESS = HttpStatus(200)
+)
+
+func NewContext(writer http.ResponseWriter, r *http.Request) *Context {
+	return &Context{Writer: writer, Req: r, Path: r.URL.Path, StatusCode: SUCCESS}
+}
+
+func (c *Context) HTML(ok int, s string) {
+	c.Writer.Header().Set("Content-Type", "text/html")
+	c.Writer.WriteHeader(ok)
+	c.Writer.Write([]byte(s))
+}
+
+func (c *Context) String(ok int, s string, i interface{}, path string) {
+	c.Writer.Header().Set("Content-Type", "text/plain")
+	c.Writer.WriteHeader(ok)
+	fmt.Fprintf(c.Writer, s, i, path)
+}
+
+func (c *Context) JSON(ok int, i map[string]interface{}) {
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.WriteHeader(ok)
+	marshal, err := json.Marshal(i)
+	if err != nil {
+		panic(err)
 	}
+	c.Writer.Write(marshal)
 }
 
-func (c *Context) Fail(code int, err string) {
-	c.index = len(c.handlers)
-	c.JSON(code, H{"message": err})
+func (c *Context) PostForm(s string) string {
+	return c.Req.FormValue(s)
+}
+
+func (c *Context) Query(s string) string {
+	return c.Req.URL.Query().Get(s)
 }
 
 func (c *Context) Param(key string) string {
-	value, _ := c.Params[key]
-	return value
+	return c.Params[key]
 }
 
-func (c *Context) PostForm(key string) string {
-	return c.Req.FormValue(key)
-}
+func (c *Context) Fail(i int, s string) {
 
-func (c *Context) Query(key string) string {
-	return c.Req.URL.Query().Get(key)
-}
-
-func (c *Context) Status(code int) {
-	c.StatusCode = code
-	c.Writer.WriteHeader(code)
-}
-
-func (c *Context) SetHeader(key string, value string) {
-	c.Writer.Header().Set(key, value)
-}
-
-func (c *Context) String(code int, format string, values ...interface{}) {
-	c.SetHeader("Content-Type", "text/plain")
-	c.Status(code)
-	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
-}
-
-func (c *Context) JSON(code int, obj interface{}) {
-	c.SetHeader("Content-Type", "application/json")
-	c.Status(code)
-	encoder := json.NewEncoder(c.Writer)
-	if err := encoder.Encode(obj); err != nil {
-		http.Error(c.Writer, err.Error(), 500)
-	}
-}
-
-func (c *Context) Data(code int, data []byte) {
-	c.Status(code)
-	c.Writer.Write(data)
-}
-
-func (c *Context) HTML(code int, html string) {
-	c.SetHeader("Content-Type", "text/html")
-	c.Status(code)
-	c.Writer.Write([]byte(html))
 }
